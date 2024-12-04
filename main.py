646e92ca8413e8805a812ece9301e7b06e6937e0
Aksel <aknea001@osloskolen.no>
2024-12-04 14:31:25 +0100

    added AES encryption

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ import sqlconnect
 def main():
     x = str(input(">> "))
     if x == "new":
-        title = str(input("Title: \n>> "))
-        username = str(input("Username: \n>> "))
-        password = str(input("Password: \n>> "))
+        title = str(input("Title: "))
+        username = str(input("Username: "))
+        password = str(input("Password: "))
 
         sqlconnect.insert(password, title or None, username or None)
-
 
 if __name__ == "__main__":
     main()
--- sqlconnect.py
+++ sqlconnect.py
@@ -11,7 +11,36 @@ sqlconfig = {
     "database": getenv("DATABASE")
 }
 
+def pad(s):
+    padding = 16 - len(s) % 16
+    return s + padding * chr(padding)
+
+def unpad(s):
+    padVerdi = s[-1]
+    return s[:-padVerdi]
+
 def insert(passwd, title=None, usrname=None):
+    from Crypto.Cipher import AES
+    from Crypto.Random import get_random_bytes
+
+    import base64
+
+    try:
+        with open(".key/key.bin", "rb") as f:
+            key = f.read()
+
+        with open(".key/iv.bin", "rb") as f:
+            iv = f.read()
+    except FileNotFoundError:
+        key = get_random_bytes(16)
+        iv = get_random_bytes(16)
+
+    passwd = pad(passwd).encode()
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+
+    encryptedPass = cipher.encrypt(passwd)
+    b64EncryptedPass = base64.b64encode(encryptedPass)
+
     try:
         db = mysql.connector.connect(**sqlconfig)
         cursor = db.cursor()
@@ -20,13 +49,55 @@ def insert(passwd, title=None, usrname=None):
                 VALUES\
                 (%s, %s, %s)"
         
-        cursor.execute(query, (passwd, title, usrname))
+        cursor.execute(query, (b64EncryptedPass, title, usrname))
         db.commit()
+
+        with open(".key/key.bin", "wb") as f:
+            f.write(key)
+
+        with open(".key/iv.bin", "wb") as f:
+            f.write(iv)
 
         print("Successfully inserted into db..")
     except mysql.connector.Error as e:
-        return f"sqlconnect insert ERROR: {e}"
+        print(f"sqlconnect insert ERROR: {e}")
     finally:
         if db != None and db.is_connected():
             cursor.close()
             db.close()
+
+def delete():
+    from Crypto.Cipher import AES
+    from Crypto.Random import get_random_bytes
+
+    import base64
+
+    try:
+        db = mysql.connector.connect(**sqlconfig)
+        cursor = db.cursor()
+
+        query = "SELECT password FROM passwds WHERE id = %s"
+        cursor.execute(query, (6, ))
+        x = cursor.fetchone()[0]
+
+        b64password = base64.b64decode(x)
+        
+        with open(".key/key.bin", "rb") as f:
+            key = f.read()
+
+        with open(".key/iv.bin", "rb") as f:
+            iv = f.read()
+
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+        decrypted = cipher.decrypt(b64password)
+        decrypted = unpad(decrypted).decode()
+
+        print(decrypted)
+    except mysql.connector.Error as e:
+        print(e)
+    finally:
+        if db != None and db.is_connected():
+            cursor.close()
+            db.close()
+
+delete()

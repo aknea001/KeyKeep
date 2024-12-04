aaf834c7861059d630e92f5f43f5f73d4d3ab0e0
aknea001 <aknea001@osloskolen.no>
2024-12-04 18:05:04 +0100

    added get func to main loop (and made a main loop)

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import sqlconnect
 
 def main():
-    x = str(input(">> "))
-    if x == "new":
-        title = str(input("Title: "))
-        username = str(input("Username: "))
-        password = str(input("Password: "))
+    while True:
+        x = str(input("\n\n>> "))
+        if x == "exit":
+            break
+        elif x == "new":
+            title = str(input("Title: "))
+            username = str(input("Username: "))
+            password = str(input("Password: "))
 
-        sqlconnect.insert(password, title or None, username or None)
+            sqlconnect.insert(password, title or None, username or None)
+        elif x == "get":
+            try:
+                pID = int(input("ID: "))
+                sqlconnect.get(pID)
+            except ValueError:
+                print("Has to be a whole number..")
 
 if __name__ == "__main__":
     main()
